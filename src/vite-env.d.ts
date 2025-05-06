97db1e4834d2441c6ccce8405605b9ab6ad4a504
Artie <artierosser@gmail.com>
2025-05-06 17:28:00 +0000

    base install

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
+/** @format */
+
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ANTHROPIC_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
