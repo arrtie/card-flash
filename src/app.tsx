538c35f81e1b4902c506ec21f0b57f7de40bc39c
Artie <artierosser@gmail.com>
2025-05-08 21:01:48 +0000

    updated some naming

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import ManualFlashcard from "./feature/ManualFlashcard.tsx";
 export function App() {
   return (
     <main>
-      <h1>Vite & Preact template</h1>
+      <h1>{import.meta.env.VITE_PAGE_TITLE}</h1>
       <DataConverter />
       <ManualFlashcard />
     </main>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_ANTHROPIC_API_KEY: string;
+  readonly VITE_PAGE_TITLE: string;
 }
 
 interface ImportMeta {
